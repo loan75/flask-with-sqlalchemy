9fbae5ba5f3ba444fca13fd465b14fb4ba7a25b7
loan75 <loan.harrouin@gmail.com>
2020-10-07 12:00:05 +0200

    CRUD product

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,7 @@
 # pylint: disable=missing-docstring
 
 from config import Config
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow  # NEW LINE (Order is important here!)
 
@@ -12,9 +12,48 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 from models import Product
-from schemas import many_product_schema
+from schemas import many_product_schema, one_product_schema
 
 @app.route(f'{BASE_URL}/products', methods=['GET'])
 def get_many_product():
     products = db.session.query(Product).all() # SQLAlchemy request => 'SELECT * FROM products'
     return many_product_schema.jsonify(products), 200
+
+@app.route(f'{BASE_URL}/products', methods=['POST'])
+def create_one_product():
+    content = request.json
+    name = content.get('name', None)
+    description = content.get('description', '')
+    if name is None:
+        return {}, 404
+    new_product = Product(name=name, description=description)
+    db.session.add(new_product)
+    db.session.commit()
+    return one_product_schema.jsonify(new_product), 201
+
+@app.route(f'{BASE_URL}/products/<int:id>', methods=['GET'])
+def get_one_product(id):
+    product = db.session.query(Product).get(id)
+    return one_product_schema.jsonify(product), 200
+
+@app.route(f'{BASE_URL}/products/<int:id>', methods=['DELETE'])
+def delete_one_product(id):
+    db.session.query(Product).filter_by(id=id).delete()
+    db.session.commit()
+    return {}, 204
+
+@app.route(f'{BASE_URL}/products/<int:id>', methods=['PATCH'])
+def update_one_product(id):
+    product = db.session.query(Product).get(id)
+    if product is None:
+        return {}, 404
+    content = request.json
+    name = content.get('name', None)
+    description = content.get('description', None)
+    if name is not None and name != '':
+        product.name = name
+    if description is not None and description != '':
+        product.description = description
+    db.session.add(product)
+    db.session.commit()
+    return one_product_schema.jsonify(product), 201
